ed534d59182cfe786a6796e50bfd6c2c6d879d07
BhavneetS <bhavneet.singh@hotmail.com>
2026-06-26 15:19:25 +0530

    Refactor Navigation component to use NavLink for active route styling

--- src/pages/Components/Navigation.jsx
+++ src/pages/Components/Navigation.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { /* Link, */ NavLink } from "react-router-dom"
 import classes from './Navigation.module.css'
 
 export default function Navigation() {
@@ -6,8 +6,13 @@ export default function Navigation() {
         <header className={classes.header}>
             <nav>
                 <ul className={classes.list}>
-                    <li> <Link to="/">Home</Link></li>
-                    <li><Link to="/products">Products</Link></li>
+                    {/* 
+                        The path is compared from the start to identify the active route. 
+                        As "/" is a subset of "/products", both routes will be marked as active when the /products route is Selected. 
+                        In order to avoid this, we can use the "end" attribute that makes sure that the route is marked as active only if the end of the current route matches the path.
+                    */}
+                    <li><NavLink to="/" className={({isActive})=>(isActive ? classes.active : undefined)} end>Home</NavLink></li>
+                    <li><NavLink to="/products" className={({isActive})=>(isActive ? classes.active : undefined)}>Products</NavLink></li>
                 </ul>
             </nav>
         </header>
